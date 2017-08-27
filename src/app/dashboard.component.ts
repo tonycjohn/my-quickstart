265d7e54ef8759349158f4ff03748b86050131df
Tony John <tony.john@nielsen.com>
2017-08-27 18:19:58 -0500

    added Dashboard Component. Sharing same service from Dashboad and Heroes Components.

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -1,8 +1,20 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+import {Hero} from './hero';
+import {HeroService} from './hero.service';
 
 @Component({
   selector:'my-dashboard',
-  template:`<h3>My Dashboard</h3>`
+  templateUrl:'./dashboard.component.html'
 })
 
-export class DashboardComponent{}
+export class DashboardComponent implements OnInit{
+  heroes:Hero[]=[];
+  callCounter=0;
+  constructor(private heroService:HeroService){}
+
+  ngOnInit():void{
+    this.heroService.getHeroes().then(heroes=>this.heroes=heroes.slice(1,5));
+    this.callCounter++;
+  }
+}
